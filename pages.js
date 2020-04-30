ef5ec79d26bd9ee5698628e4fd786961a8b24944
Justin <33503280+JKongman@users.noreply.github.com>
2020-04-30 11:50:51 -0400

    Add files via upload

--- pages.js
+++ pages.js
@@ -6,70 +6,44 @@ const hash = require('bcrypt');
 
 
 const user = new User();
-//groupPost
-router.get('/group',(req,res)=>{
+
+router.get('/GroupPost',(req,res)=>{
+    console.log("inside GroupPost")
     if(req.session && req.session.user){
-        //console.log(req.session.user)
-        user.find(req.session.user.Email,(result)=>{
-            if(result){
-                res.render('home-form')
-                return;
-            }
-            res.redirect('/');
+        user.getPosts((result)=>{
+            //console.log(result)
+            result.reverse()
+            res.render('home-form',{posts:result})
         })
     }else
     {
         res.redirect('/');
     }
-    
+    console.log("End GroupPost")
 })
-// comment
+// go table comment
 router.get('/comment',(req,res)=>{
-    console.log('in comment')
-    if(req.session && req.session.user){
-        console.log(req.session.user)
-        user.find(req.session.user.Email,(result)=>{
-            if(result){
-                res.render('partials/comment-form');
-                return;
-            }
-            res.redirect('/');
-        })
-    }else
-    {
-        res.redirect('/');
-    }
+    res.send("Still work")
+    // console.log('in comment')
+    // if(req.session && req.session.user){
+    //     console.log(req.body)
+    //     user.find(req.session.user.Email,(result)=>{
+    //         if(result){
+    //             res.render('partials/comment-form');
+    //             return;
+    //         }
+    //         res.redirect('/');
+    //     })
+    // }else
+    // {
+    //     res.redirect('/');
+    // }
 });
-router.post('/submitcomment',(req,res)=>{
-    console.log('in submit comment')
-    if(req.session.user){
-        //console.log(req.session.user.UserID)
-        user.findPostID(req.session.user.UserID,(result)=>{
-            result = JSON.stringify(result[0]);
-            result= JSON.parse(result);
-            //data
-            UserID = req.session.user.UserID
-            PostID = result.PostID
-            CommentContent = req.body.addComment
-            TimeOfComment = new Date()
-             //Insert the user into the database
-            sql = "INSERT INTO comments (UserID, PostID, CommentContent, TimeOfComment) VALUES (?, ?, ?, ?)";
-            data = [UserID, PostID, CommentContent, TimeOfComment];
 
-            con.query(sql, data, (err, result) => {
-                if(err) throw err;
 
-                //REGISTRATION SUCCESSFUL
-                res.redirect("/group");
-            })
-        })
-    }else{
-        res.redirect('/')
-    }
-
-})
+//get create Post
 router.get('/createpost',(req,res)=>{
-    console.log('in post')
+    console.log('in table create post')
     if(req.session && req.session.user){
         //console.log(req.session.user)
         user.find(req.session.user.Email,(result)=>{
@@ -77,42 +51,45 @@ router.get('/createpost',(req,res)=>{
                 res.render('partials/post-form')
                 return;
             }
-            res.redirect('/');
+            res.redirect('/GroupPost');
         })
     }else
     {
         res.redirect('/');
     }
+    console.log("End table create post")
 })
-//get create Post
 router.post('/submitpost',(req,res)=>{
     console.log("in submit post")
     if(req.session.user.UserID){
         var UserID = req.session.user.UserID
         var PostContent = req.body.addComment
         var TimeOfComment= new Date()
+        var FullName = req.session.user.FirstName +' '+ req.session.user.LastName
        // console.log(UserID+" "+PostContent+"time:"+TimeOfComment)
         //Insert the user into the database
-        sql = "INSERT INTO posts (UserID, PostContent, TimeOffPost) VALUES ( ?, ?, ?)";
-        data = [UserID, PostContent,TimeOfComment];
+        sql = "INSERT INTO posts (UserID, PostContent, FullName, TimeOffPost) VALUES ( ?, ?, ?, ?)";
+        data = [UserID, PostContent, FullName,TimeOfComment];
     
          con.query(sql, data, (err, result) => {
              if(err) throw err;
     
              //SUccess
-             console.log("success")
-             res.redirect("/group");
+             
+             res.redirect("/GroupPost");
          })
     }else{
         res.redirect("/");
     }
 
-    
+    console.log("end submit Post")
 })
+
 // Index Page
 router.get('/', function(req, res){
     let user = req.session.user;
     if (user) {
+        console.log(req.session.user)
         res.redirect('/home');
         return;
     }
@@ -134,7 +111,7 @@ router.get('/home', (req, res, next)=>{
 router.post('/login', (req, res, next)=> {
     user.login(req.body.username, req.body.password, function(result){
         if(result) {
-           //console.log("result",result)
+           // console.log("result",result)
             req.session.user = result;
             req.session.opp = 1;
 
@@ -144,7 +121,7 @@ router.post('/login', (req, res, next)=> {
             }
             else{
                 req.flash('Logged in as :'+result.username);
-                res.redirect('/group');
+                res.redirect('/home');
             }
 
             
@@ -323,8 +300,8 @@ router.post("/register", (req, res) => {
         else
         {
             //Insert the user into the database
-            sql = "INSERT INTO accounts (FirstName, LastName, Email, Passwd, NumVotes) VALUES (?, ?, ?, ?, ?)";
-            data = [first_name, last_name, email, password, 0];
+            sql = "INSERT INTO accounts (FirstName, LastName, Email, Passwd) VALUES (?, ?, ?, ?)";
+            data = [first_name, last_name, email, password];
 
             con.query(sql, data, (err, result) => {
                 if(err) throw err;
@@ -338,7 +315,152 @@ router.post("/register", (req, res) => {
     })    
 })
 
+//profile
+router.get("/profile", (req, res) => {
+    console.log('profile')
+    con.query(`select * from accounts where UserID=${req.session.user.UserID}`, (err,result) => {
+        if(err) throw err;
+        res.render('profile', {firstName : result[0].FirstName, lastName : result[0].LastName,
+         email : result[0].Email, bio : result[0].Bio, pfp : result[0].ProfilePicture, boo:true} );
+    })
+    })
 
+//other profile
+router.get("/otherprofile", (req, res) => {
+
+    con.query(`select * from accounts where UserID=2`, (err,result) => {
+        if(err) throw err;
+        res.render('profile.ejs', {firstName : result[0].FirstName, lastName : result[0].LastName,
+         email : result[0].Email, bio : result[0].Bio, pfp : result[0].ProfilePicture, boo: false} );
+    })
+})
+
+
+//edit profile page
+router.get("/editprofile", (req, res) => {
+    con.query(`select * from accounts where UserID=${req.session.user.UserID}`, (err,result) => {
+        if(err) throw err;
+        res.render('editprofile', {firstName : result[0].FirstName, lastName : result[0].LastName,
+            email : result[0].Email, bio : result[0].Bio, pfp : result[0].ProfilePicture} );
+    })
+})
+
+//update profile
+router.post("/updateprofile", (req, res) => {
+
+    /*prepared sql statement
+    gets data from parser and saves it for sql statement in variable data
+    executes prepared sql statement*/
+    let sql = `update accounts set FirstName = ?, LastName=?, Email=?, Bio=? where UserID=${req.session.user.UserID};`
+    let data = [req.body.fName, req.body.lName, req.body.email, req.body.bio]
+    con.query(sql, data, (err,result) => {
+        if(err) throw err;
+        res.redirect('/profile');
+    })
+})
+
+
+//settings
+router.get("/settings", (req, res) => {
+        res.render('settings')
+    })
+
+//account deleted
+router.get("/deletedaccount", (req, res) => {
+    con.query(`delete from accounts where UserID=${req.session.user.UserID}`, (err,result) => {
+        if(err) throw err;
+        res.render('index');
+    })
+})
+
+//Change/Leave Group
+router.get('/changegroup', (req, res) => {
+    let id = req.session.user.UserID;
+    con.query('UPDATE groupp set NumberOfPeopleInGroup = NumberOfPeopleInGroup - 1 where GroupID = (SELECT GroupID FROM accounts WHERE UserID = ?)', id, (err, result) => {
+        if (err) throw err;
+        con.query('UPDATE accounts set GroupID = Null WHERE UserID = ?', id, (err, result) => {
+            if (err) throw err;
+            res.redirect("/regroup");          
+        });
+    });
+});
+//Delete Post with comments
+router.get('/deletepost/:eventID', (req, res) => {
+    console.log(req.params.eventID); console.log(req.session.user.UserID);
+    let sql = 'DELETE FROM comments where PostID = ?';
+    let data = [req.params.eventID];
+    con.query(sql, data, (err, result) => {
+        if (err) throw err;
+        sql = 'DELETE FROM posts where PostID = ? and UserID = ?';
+        data = [req.params.eventID, req.session.user.UserID];
+        con.query(sql, data, (err, result) => {
+            if (err) console.log(err);
+            console.log(result);     
+        });
+    }); 
+});
+
+
+
+//admin page
+router.get("/adminpage", (req, res) => {
+    res.render('adminpage')
+    })
+
+
+//stuff for profile pictures
+const multer = require('multer')
+router.use(express.static('./public'))
+const path = require('path')
+
+const storage = multer.diskStorage({
+    //renames the images and stores them in the public folder
+    destination: './public',
+    filename: function(req, file, cb){
+        cb(null,file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
+    }
+})
+const upload = multer({
+    storage: storage
+}).single('img');
+
+//new pfp
+router.post("/newpfp", (req, res) => {
+    upload(req, res, (err)=>{
+        //sql to change the pfp
+        let sql = `update accounts set ProfilePicture = ? where UserID=${req.session.user.UserID};`
+        let data = [req.file.filename]
+        con.query(sql, data, (err,result) => {
+        if(err) throw err;
+
+        //redirects to profile page
+        res.redirect('/profile');
+    })
+    })
+    });
+
+    router.get('/group',(req,res)=>{
+        if(req.session && req.session.user){
+            let sql = `SELECT GroupID from accounts WHERE UserID=${req.session.user.UserID};`
+            con.query(sql, (err, result)=>{
+                if(err) throw err;
+                //console.log(result)
+    
+                sql = `SELECT FirstName, LastName, Email, Bio, ProfilePicture, UserID from accounts WHERE GroupID = ${result[0].GroupID} and UserID <> ${req.session.user.UserID};`
+                con.query(sql, (err, result2)=>{
+                //console.log(result2)
+                res.render('group', {users: result2, numMembers: result2.length})
+    
+                })
+    
+                })}else{
+                    res.redirect('/');
+                }
+                })
+
+
+
+                // VOTE OUT FORM
 // VOTE USER OUT OF GROUP 
 router.post("/voteout", (req, res) => {
 
@@ -353,20 +475,22 @@ router.post("/voteout", (req, res) => {
     // GET USER ID
     var voterid = req.session.user.userID;
 
-
     // GET USERNAME OF VICTIM
     var victimid = req.body.email;
-    //console.log(victimid);
-
+    console.log(victimid);
     // CHECK THAT THE USER DOESNT VOTE THEMSELVES OUT
     if (req.session.user.email == req.body.email) {
         res.redirect('/group');
+        
     }
+    //console.log(req.body.email);
+    //console.log(req.session.user.GroupID);
 
     // INSERT A VOTE INTO THE USER NUMVOTES
-    let sql = "UPDATE accounts SET NumVotes = NumVotes + 1 WHERE Email = ?";
+    let sql = "UPDATE accounts SET NumVotes=COALESCE(NumVotes, 0) + 1 WHERE Email = ?";
     let data = [victimid];
-
+    
+    
         // CHECK FOR ERRORS IN DATA ENTRY
             con.query(sql, data, (err, result) => {
                 if(err) throw err;
@@ -390,34 +514,52 @@ router.post("/voteout", (req, res) => {
         
         // CHECK NUM VOTES ON VICTUM AND COMPARE TO GROUP USERS
             // IF GREATER THAN 50% FOR NOW SET 6 [req.session.GroupID.NumOfPoepleInGroup]
-        if(result.NumVotes >= 6) {
-                //console.log("Hello");
+    //SQL GET THE NUMBER OF PEOPLE IN THE GROUP
+
+    sql = `SELECT NumberOfPeopleInGroup from groupp WHERE GroupID=${req.session.user.GroupID};`
+    con.query(sql, (err, numPG)=>{
+        if(err) throw err;
+
+        numPG = JSON.stringify(numPG[0]);
+        numPG = JSON.parse(numPG);
+        //console.log(numPG.NumberOfPeopleInGroup);
+        //console.log('NUM'+result.NumVotes);
+        
+       if(result.NumVotes + 1 >= (numPG.NumberOfPeopleInGroup * .5)) {
+                console.log("Hello");
+                console.log(result.NumVotes);
+                
+                
             // DELETE VICTIUM NUM VOTES & SET VICTIM GROUP TO NULL
-        sql = "UPDATE accounts SET NumVotes = null, GroupID = null WHERE Email = ?"
+        sql = "UPDATE accounts SET NumVotes = null, GroupID = null WHERE Email = ?";
         data = [victimid];
 
             // DELETE USER NUM VOTES AGAINST VICTIM
             con.query(sql, data, (err, result) => {
                 if(err) throw err;
                 // SET BACK TO NULL
-                //console.log("3" +result);
+                console.log("3" +result);
     
             // REDUCE GROUP SIZE TO -1
             sql = "UPDATE groupp SET NumberOfPeopleInGroup = NumberOfPeopleInGroup - 1 WHERE GroupID = ?";
-            data = [req.session.groupID];
+            data = [req.session.user.GroupID];
 
             con.query(sql, data, (err, result) => {
                 if(err) throw err;
-                console.log("4" + result);
+                console.log(result);
             // REDIRECT TO REGROUP
-            res.redirect('/group');
+           
 
             });
         });
+        
     }
+
     });
+})
 }
 });
+
 
 
 module.exports = router;
